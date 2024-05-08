93a3fa636e302d611e6851ca09d33e0cc09b42a4
MingYu <zoozxc456@gmail.com>
2024-05-08 11:06:41 +0800

    feat: add join lab

---
+++ LinqTraining.Test/JoinLabTest.cs
@@ -0,0 +1,35 @@
+﻿using ExpectedObjects;
+using LinqTraining.Labs;
+
+namespace LinqTraining.Test;
+
+[TestFixture]
+public class JoinLabTest
+{
+    private readonly JoinLab _joinLab;
+
+    public JoinLabTest()
+    {
+        _joinLab = new JoinLab();
+    }
+
+    [Test]
+    public void GetBuyersByTotalHigherThen500()
+    {
+        var expected = new List<string> { "Tim", "Tracy", "Tina", "Ben" }.ToExpectedObject();
+
+        var result = _joinLab.GetBuyersByTotalHigherThen500();
+
+        expected.ShouldEqual(result);
+    }
+
+    [Test]
+    public void GetBuyersInWhichGroup()
+    {
+        var expected = new List<string> { "Tim in Normal", "Tracy in VIP", "Mary in Normal", "Tina in Normal", "Jason in Normal", "Ben in Normal" }.ToExpectedObject();
+
+        var result = _joinLab.GetBuyersInWhichGroup();
+
+        expected.ShouldEqual(result);
+    }
+}
---
+++ LinqTraining/Labs/JoinLab.cs
@@ -0,0 +1,34 @@
+﻿using LinqTraining.Data;
+
+namespace LinqTraining.Labs;
+
+public class JoinLab
+{
+    private readonly List<Product> _products;
+    private readonly List<Order> _orders;
+    private readonly List<OrderGrouping> _orderGroupings;
+
+    public JoinLab()
+    {
+        _products = new ProductService().GetProducts();
+        _orders = new OrderService().GetOrders();
+        _orderGroupings = new OrderGroupingService().GetOrderGroupings();
+    }
+
+    public List<string> GetBuyersByTotalHigherThen500()
+    {
+        var result = new List<string>();
+
+        // TODO: Product Join Order, and Get Buyer whose Total is higher Then 500
+
+        return result;
+    }
+
+    public List<string> GetBuyersInWhichGroup()
+    {
+        var result = new List<string>();
+        // TODO: Order Join OrderGrouping, and Get Buyer in Which Group (Output template: Buyer in GroupName)
+
+        return result;
+    }
+}
